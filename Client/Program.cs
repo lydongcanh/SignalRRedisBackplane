5da291e7037842a7831667ec90344a8eb2635815
lydongcanh3p <canh.ly@partners.3plearning.com>
2021-05-30 17:29:09 +0700

    Initialize commit.

---
+++ Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(args);
+        }
+    }
+}
---
+++ Server/Hubs/TickHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs
+{
+    public class TickHub : Hub
+    {
+        public async Task Tick(int time)
+        {
+            await Clients.All.SendAsync("OnTickReceived", time);
+        }
+    }
+}
